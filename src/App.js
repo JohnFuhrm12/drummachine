5eb51cc1e43ef4612c3689663d5558e5774a40bb
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-01-09 18:04:48 -0300

    ClassNames and Ids setUp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import './App.css';
 
 function App() {
+
+  function handleClick() {
+
+  }
+
   return (
     <>
     <div className="page">
       <div id='drum-machine'>
         <div className='pads'>
           <div className='row'>
-            <button className='drum-pad'>Q</button>
-            <button className='drum-pad'>W</button>
-            <button className='drum-pad'>E</button>
+            <button onClick={handleClick} className='drum-pad' id='Heater-1'><audio src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' id='Q' className='clip'/>Q</button>
+            <button onClick={handleClick} className='drum-pad' id='sfsasda'><audio src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' id='W' className='clip'/>W</button>
+            <button onClick={handleClick} className='drum-pad' id='sfsasda-1'><audio src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' id='E' className='clip'/>E</button>
           </div>
           <div className='row'>
-            <button className='drum-pad'>A</button>
-            <button className='drum-pad'>S</button>
-            <button className='drum-pad'>D</button>
+            <button onClick={handleClick} className='drum-pad' id='sfsasda-1'><audio src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' id='A' className='clip'/>A</button>
+            <button onClick={handleClick} className='drum-pad' id='sfsasda-1'><audio src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' id='S' className='clip'/>S</button>
+            <button onClick={handleClick} className='drum-pad' id='sfsasda-1'><audio src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' id='D' className='clip'/>D</button>
           </div>
           <div className='row'>
-            <button className='drum-pad'>Z</button>
-            <button className='drum-pad'>X</button>
-            <button className='drum-pad'>C</button>
+            <button onClick={handleClick} className='drum-pad' id='sfsasda-1'><audio src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' id='Z' className='clip'/>Z</button>
+            <button onClick={handleClick} className='drum-pad' id='sfsasda-1'><audio src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' id='X' className='clip'/>X</button>
+            <button onClick={handleClick} className='drum-pad' id='sfsasda-1'><audio src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' id='C' className='clip'/>C</button>
           </div>
         </div>
         <div id='display'>
