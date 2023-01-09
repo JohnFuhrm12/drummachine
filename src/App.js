f48cfa925414ada7fb1e1d5884ce1509cda2db75
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-01-09 17:51:58 -0300

    Genesis

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="page">
+      <div id='drum-machine'>
+        <div className='pads'>
+          <div className='row'>
+            <button className='drum-pad'>Q</button>
+            <button className='drum-pad'>W</button>
+            <button className='drum-pad'>E</button>
+          </div>
+          <div className='row'>
+            <button className='drum-pad'>A</button>
+            <button className='drum-pad'>S</button>
+            <button className='drum-pad'>D</button>
+          </div>
+          <div className='row'>
+            <button className='drum-pad'>Z</button>
+            <button className='drum-pad'>X</button>
+            <button className='drum-pad'>C</button>
+          </div>
+        </div>
+        <div id='display'>
+          <h2>Heater 1</h2>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
 
